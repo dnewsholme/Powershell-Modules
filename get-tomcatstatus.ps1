42079649cf803de43c25e4253e4b2d963def35bf
Daryl Bizsley <dbizsley@googlemail.com>
2015-08-25 21:26:27 +0100

    Update get-tomcatstatus.ps1

--- get-tomcatstatus.ps1
+++ get-tomcatstatus.ps1
@@ -1,37 +1,44 @@
 function Get-TomcatStatus{
     param(
+        [ValidateSet("http","https")]$protocol,  
         $tomcatserver = "localhost",
         $tomcatport = 8080,
-        $tomcatuser,
-        $tomcatpassword
+        [PSCredential]$Credential
     )
-    #Get Credentials into the right format
-    $tomcatpassword = ConvertTo-SecureString -string $tomcatpassword -asplaintext -force
-    $cred = New-object -TypeName System.Management.Automation.PSCredential -argumentlist ($tomcatuser,$tomcatpassword)
 
     #Invoke the restinterface 
-    $TomcatStats = Invoke-RestMethod -Uri http://$tomcatserver`:$tomcatport/manager/status?XML=true -Credential $cred
-    
-    $threads = $TomcatStats.GetElementsByTagName("threadInfo") | ? {$_.currentThreadCount -ne 0}
+    try { 
+        $TomcatStats = Invoke-RestMethod -Uri ('{0}://{1}:{2}/manager/status?XML=true' -f $protocol,$tomcatserver,$tomcatport) -Credential $Credential -ErrorAction Stop
+    }
+    catch [System.Net.WebException]{
+        return "Unable to connect to host"
+        break
+    }
+
+    $threads = $TomcatStats.GetElementsByTagName("threadInfo") 
     
     $Memory = $TomcatStats.GetElementsByTagName("memory") 
     
-    $requestInfo = $TomcatStats.GetElementsByTagName("requestInfo") | ? {$_.bytessent -ne 0}
-
+    $requestInfo = $TomcatStats.GetElementsByTagName("requestInfo")
+    
     #Populate output object
     $Output = New-object PsObject
+
+    $parms = @{
+        Type = "noteproperty"
+    }
     #Memory
-    $output | add-member -type noteproperty -name "Memory_Used" -Value $memory.total
-    $output | add-member -type noteproperty -name "Memory_free" -Value $memory.free
-    $output | add-member -type noteproperty -name "Memory_Max" -Value $memory.max
+    $output | add-member @parms -name "Memory_Used" -Value $memory.total
+    $output | add-member @parms -name "Memory_free" -Value $memory.free
+    $output | add-member @parms -name "Memory_Max" -Value $memory.max
     #threads
-    $output | add-member -type noteproperty -name "Current_Threads_busy" -Value $threads.currentThreadsBusy
-    $output | add-member -type noteproperty -name "Current_Thread_Count" -Value $threads.currentThreadCount
-    $output | add-member -type noteproperty -name "Max_Threads" -Value $threads.MaxThreads
+    $output | add-member @parms -name "Current_Threads_busy" -Value $threads.currentThreadsBusy
+    $output | add-member @parms -name "Current_Thread_Count" -Value $threads.currentThreadCount
+    $output | add-member @parms -name "Max_Threads" -Value $threads.MaxThreads
     #requestInfo
-    $output | add-member -type noteproperty -name "Request_Max_ProcessingTime_ms" -Value $requestInfo.maxTime
-    $output | add-member -type noteproperty -name "Request_Count" -Value $requestInfo.requestcount
-    $output | add-member -type noteproperty -name "ProcessingTime_total_s" -Value $requestInfo.processingTime
+    $output | add-member @parms -name "Request_Max_ProcessingTime_ms" -Value $requestInfo.maxTime
+    $output | add-member @parms -name "Request_Count" -Value $requestInfo.requestcount
+    $output | add-member @parms -name "ProcessingTime_total_s" -Value $requestInfo.processingTime
 
     Return $output
 }
